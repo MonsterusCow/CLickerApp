c82f8234e95a58a8ef335627e93c1c14fc0906c3
MonsterusCow <ryan.stark26@d155.org>
2024-09-24 08:07:39 -0500

    Initial Commit

---
+++ CLickerAppTests/CLickerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CLickerAppTests.swift
+//  CLickerAppTests
+//
+//  Created by RYAN STARK on 9/24/24.
+//
+
+import Testing
+@testable import CLickerApp
+
+struct CLickerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
