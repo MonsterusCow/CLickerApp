bd97924854f916fa091f12147574299f76cbe96e
MonsterusCow <ryan.stark26@d155.org>
2024-09-26 08:10:30 -0500

    uggggggg

--- CLickerApp/ShopViewController.swift
+++ CLickerApp/ShopViewController.swift
@@ -10,18 +10,86 @@ import UIKit
 class ShopViewController: UIViewController {
 
     var scrap = 0
+    var money = 0
+    var worth = 1
     @IBOutlet weak var scrapAmount: UILabel!
+    @IBOutlet weak var moneyLabel: UILabel!
+    @IBOutlet weak var scrapLabel: UILabel!
+    @IBOutlet weak var ringLabel: UILabel!
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-    }
+        scrapLabel.text = "Scrap: \(scrap)"
+        moneyLabel.text = "Money: \(money)"
 
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        var nvc = segue.destination as! ViewController
-        nvc.scrap = scrap
     }
+    
+    @IBAction func ringButton(_ sender: UIButton) {
+        money = money + (scrap * worth)
+        Info.moneyy = money
+        scrap = 0
+        Info.scrapp = 0
+        scrapLabel.text = "Scrap: \(scrap)"
+        moneyLabel.text = "Money: \(money)"
+    }
+    
+    @IBAction func TenButton(_ sender: UIButton) {
+        if money >= 10 {
+            worth = worth + 1
+            money = money - 10
+            Info.moneyy = money
+            moneyLabel.text = "Money: \(money)"
+        } else {
+            broke(alertName: "You dont have enough money")
+        }
+    }
+    @IBAction func HunButton(_ sender: UIButton) {
+        if money >= 100 {
+            worth = worth * 2
+            money = money - 100
+            Info.moneyy = money
+            moneyLabel.text = "Money: \(money)"
+        }else {
+            broke(alertName: "You dont have enough money")
+        }
+    }
+    @IBAction func WinButton(_ sender: UIButton) {
+        if money >= 10000{
+            win(alertName: "Wowie u became ceo :)")
+            money = money - 10000
+            Info.moneyy = money
+            moneyLabel.text = "Money: \(money)"
+
+        }else {
+            broke(alertName: "You dont have enough money")
+        }
+    }
+    
+    func broke(alertName: String)
+        {
+            let alert = UIAlertController(title: "Not enough money", message: alertName, preferredStyle: UIAlertController.Style.alert)
+            let alertAction = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil)
+            
+            alert.addAction(alertAction)
+            
+            self.present(alert, animated: true)
+            
+        }
+    
+    func win(alertName: String)
+        {
+            let alert = UIAlertController(title: "You Win", message: alertName, preferredStyle: UIAlertController.Style.alert)
+            let alertAction = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil)
+            
+            alert.addAction(alertAction)
+            
+            self.present(alert, animated: true)
+            
+        }
+    
+
+    
     
 }
--- CLickerApp/ViewController.swift
+++ CLickerApp/ViewController.swift
@@ -4,7 +4,10 @@
 //
 //  Created by RYAN STARK on 9/24/24.
 //
-
+class Info{
+    static var scrapp = 0
+    static var moneyy = 0
+}
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,34 +15,102 @@ class ViewController: UIViewController {
     @IBOutlet weak var scrapPicture: UIImageView!
     @IBOutlet weak var scrapAmount: UILabel!
     @IBOutlet weak var moneyAmount: UILabel!
-    var mooney = 0
+    var money = 0
     var scrap = 0
-    
+    var blowup = false
+    @IBOutlet weak var stunImage: UIImageView!
+    @IBOutlet weak var stunThing: UIButton!
     
     var addAmount = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        stunImage.isHidden = true
+        stunThing.isEnabled = false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        money = Info.moneyy
+        scrap = Info.scrapp
+        scrapAmount.text = "Scrap: \(scrap)"
+        moneyAmount.text = "Money: \(money)"
+
+    }
+
+    @IBAction func scrappedTapped(_ sender: UIButton) {
+        if !blowup{
+            scrap = scrap + addAmount
+            scrapAmount.text = "Scrap: \(scrap)"
+            rollScrap()
+        } else {
+            scrap = 0
+            scrapAmount.text = "Scrap: \(scrap)"
+            scrapPicture.image = UIImage(named: "ship")
+            blowUped(alertName: "Oops u hit a landmine, you lost all your scrap")
+            stunImage.isHidden = true
+            stunThing.isEnabled = false
+            blowup = false
+        }
+        
 
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        scrapAmount.text = "Scrap: \(scrap)"
+    @IBAction func stunButton(_ sender: UIButton) {
+        blowup = false
+        rollScrap()
+        stunImage.isHidden = true
+        stunThing.isEnabled = false
     }
-
-    @IBAction func cookieTapped(_ sender: UIButton) {
-        scrap = scrap + addAmount
-        scrapAmount.text = "Scrap: \(scrap)"
-
+    
+    
+    
+    func rollScrap(){
+        var random = Int.random(in: 0...14)
+        var another = Int.random(in: 1...50)
+        if another == 1 {
+            random = 15
+        }
+        print(random)
+        switch random{
+        case 0: scrapPicture.image = UIImage(named: "Airhorn")
+        case 1: scrapPicture.image = UIImage(named: "BeehiveIcon")
+        case 2: scrapPicture.image = UIImage(named: "Bell")
+        case 3: scrapPicture.image = UIImage(named: "BigBoltIcon")
+        case 4: scrapPicture.image = UIImage(named: "Bottles")
+        case 5: scrapPicture.image = UIImage(named: "Candy")
+        case 6: scrapPicture.image = UIImage(named: "Clown")
+        case 7: scrapPicture.image = UIImage(named: "Comedy_mask")
+        case 8: scrapPicture.image = UIImage(named: "coookie")
+        case 9: scrapPicture.image = UIImage(named: "Egg_beater")
+        case 10: scrapPicture.image = UIImage(named: "Fancy_lamp")
+        case 11: scrapPicture.image = UIImage(named: "Flask")
+        case 12: scrapPicture.image = UIImage(named: "Hair_dryer")
+        case 13: scrapPicture.image = UIImage(named: "Large_axlepng")
+        case 14: scrapPicture.image = UIImage(named: "Apparatus")
+        default: scrapPicture.image = UIImage(named: "landmine")
+            blowup = true
+            stunImage.isHidden = false
+            stunThing.isEnabled = true
+        }
     }
+    func blowUped(alertName: String)
+        {
+            let alert = UIAlertController(title: "Boom", message: alertName, preferredStyle: UIAlertController.Style.alert)
+            let alertAction = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil)
+            
+            alert.addAction(alertAction)
+            
+            self.present(alert, animated: true)
+            
+        }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         var nvc = segue.destination as! ShopViewController
+        Info.scrapp = scrap
+        Info.moneyy = money
         nvc.scrap = scrap
-        
+        nvc.money = money
     }
-    
-    
-    
     
 
 }
